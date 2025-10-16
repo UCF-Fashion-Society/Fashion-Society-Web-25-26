6c72703d681e4ee31d735525bf3ce55aa0b36753
Carlos <lopez212318@gmail.com>
2025-10-16 02:16:56 -0400

    Events page + Navbar fix

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -37,7 +37,7 @@ export default function Navbar() {
 
                  <NavLink to = "/events" className = {({ isActive }) => ` 
                                                     ${isActive 
-                                                    ? "text-underline" 
+                                                    ? "underline" 
                                                     : "hover:underline"}`}>
                             EVENTS
                 </NavLink>
--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,6 +1,77 @@
+
+const Meetings = [
+    {
+        name: "Design Meeting",
+        date: "October 15th",
+        time: "6:00pm"
+    },
+    {
+        name: "Production Meeting",
+        date: "October 15th",
+        time: "6:00pm"
+    },
+    {
+        name: "Web Design Meeting",
+        date: "October 15th",
+        time: "6:00pm"
+    },
+    {
+        name: "Photography Meeting",
+        date: "October 15th",
+        time: "6:00pm"
+    },
+    {
+        name: "Zine Meeting",
+        date: "October 15th",
+        time: "6:00pm"
+    },
+    {
+        name: "Social Media Meeting",
+        date: "October 15th",
+        time: "6:00pm"
+    },
+    {
+        name: "Beauty Meeting",
+        date: "October 15th",
+        time: "6:00pm"
+    },
+    {
+        name: "Modeling Meeting",
+        date: "October 15th",
+        time: "6:00pm"
+    },
+    {
+        name: "Styling Meeting",
+        date: "October 15th",
+        time: "6:00pm"
+    }
+]
+
 const Events = () => {
-    return(
-        <h1>Events</h1>
+    return (
+        <div>
+            <div className="grid grid-cols-2 items-center space-x-20">
+                <img src="/public/UpcomingEvents.svg" alt="Upcoming Events" className="ml-12" width={900} height={300} />
+                <h1 className=" text-[#373333] mr-20 ">
+                    <span className="font-bold">Next GBM: </span>
+                    <span> October 20th </span>
+                    <span className="font-bold"> at </span>
+                    <span> 7:00pm </span>
+                </h1>
+            </div>
+            <div className="grid grid-cols-2 ml-28 gap-2">
+                {Meetings.map((meeting) => (
+                    <div className="text-[#373333] text-lg text-left">
+                        <span className="font-bold"> Next {meeting.name}: </span>
+                        <span className=""> {meeting.date} </span>
+                        <span className="font-bold"> at </span>
+                        <span className=""> {meeting.time} </span>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 };
+
+
 export default Events;
