9e1bc59d4b3eef2ffe32725a657c68a295be4ffe
imanimcq <imanimcquay@gmail.com>
2025-10-16 11:27:59 -0400

    completed about page, added structure to pu blic

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,18 +2,20 @@ const About = () => {
     return(
          
   <main className="flex flex-row flex-auto">
-    <div className="felx flex-col justify-center">
-      <p className="text-6xl text-[#373333] px-10 py-10">About</p>
-      <div className="flex gap-6 px-10">
-        <div className="w-64 h-64 bg-red-900 rounded-xl"></div>
-        <div className="w-64 h-64 bg-red-900 rounded-xl"></div>
+    <div className="flex flex-col justify-center">
+     <div className = "mx-10 my-8 w-132 h-40 bg-[url('/About.svg')] bg-center bg-size-[680px] bg-no-repeat"></div>
+      <div className="flex gap-10 px-16">
+        <div className="w-56 h-83 bg-[url('/pictures/polaroid.svg')]"></div>
+        <div className="w-56 h-83 bg-[url('/pictures/clownMakeup.svg')]"></div>
       </div>
     </div>
     <div className="flex flex-col">
-      <p className="text-2xl text-left font-bold text-[#373333] py-10">WE ARE AN ORGANIZATION THAT PUTS CREATIVITY AND SELF EXPRESSION ABOVE ALL ELSE </p>
-      <p className="text-l text-left text-[#373333]">We split up into 9 different committees: Design, Styling, Modeling, Beauty, Production, Zine, Photography, Social Media, and Web Design</p>
-      <p className="text-l text-left text-[#373333] px-10 py-10">The Design Committee designs and sews entirely new pieces. The Styling Committee creates outfits from thrifted and upcycled clothing to style the models. </p>
-      <p className="text-l text-left text-[#373333] px-10 py-10">The Modeling Committee walks the show. Beauty, Production, Zine, Photography, Social Media, and Web Design</p>
+      <p className="text-l font-['Inter'] text-left font-bold text-[#373333] mt-16 py-6">WE ARE AN ORGANIZATION THAT PUTS CREATIVITY AND SELF EXPRESSION ABOVE ALL ELSE </p>
+      <p className="text-l font-['Inter'] text-left text-[#373333]">Our club is split up into 9 different committees that all have different roles and responsibilities.</p>
+      <p className="text-l font-['Inter'] text-left text-[#373333]  py-6">Members have the option to join one or more committees based on their personal interests, goals, and skill sets. </p>
+      <p className="text-l font-['Inter'] text-left text-[#373333]">Each committee holds separate meeting either weekly or biweekly and we hold General Body Meetings every other Monday.</p>
+      <p className="text-l font-['Inter'] text-left text-[#373333]  py-6">We host workshops, portfolio-building opportunities, and industry exposure but also fun events like parties, tailgates, and creative challenges for everyone </p>
+       <p className="text-l font-['Inter'] text-left text-[#373333]">We strive to offer opportunities to worth together across committees, whether it’s helping with a shoot, designing a look, or planning a fundraising event.</p>
     </div>
   </main>
 
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,7 +2,7 @@
 const Contact = () => {
     return(
          <div className="flex flex-col items-start">
-            <div className = "top-10 left-5 w-185 h-64 bg-[url('/F.svg')] bg-center"></div>
+            <div className = "top-10 left-5 w-185 h-64 bg-[url('/Contact.svg')] bg-center"></div>
             <p className="mx-10 mb-8 text-[#373333] font-['Inter'] font-light text-xl">
               <span className="font-bold">INSTAGRAM:  </span>@fashionsocietyucf  
             </p>
