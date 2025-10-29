1fc208e5778828163e47ead749c159b302e40944
Carlos <lopez212318@gmail.com>
2025-10-29 18:52:56 -0400

    Bottom half of landing page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,41 @@
 export default function Home() {
   return (
-    
-      <h1> 
-       Landing Page
+    <div>
+      <h1 className="flex justify-center mx-36 mt-4">
+        We want to providing a welcoming space where students can explore styling, design, photography, modeling, or simply fashion as a form of self expression — no experience or major required.
       </h1>
-   
+      <p className="absolute top-[85%] left-[12%] font-bold"> NO EXPERIENCE NEEDED</p>
+      <h1 className="font-bold mt-16 ml-24"> EVERYONE IS WELCOME</h1>
+      <div className="grid grid-cols-2 gap-8">
+        <img
+          src="/OurMission.png"
+          alt="Our Mission"
+          className="w-150 ml-24 "
+        />
+        <img
+          src="/pictures/clownMakeup.svg"
+          alt="temp img"
+          className="ml-56 mt-8"
+        />
+      
+        <img
+          src="/pictures/clownMakeup.svg"
+          alt="temp img"
+          className="ml-44"
+        />
+        <div className="text-left mr-20 text-[17px] pb-20">
+          <h1>
+            Fashion Society at UCF is a creative collective and safe space where students of all background come together to express themselves through fashion, art, and culture. whether you dream of working in the fashion industry or simply seek a space to create, connect, and grow — our mission is to nurture individuality, build confidence, and foster authentic community through collaborative experience like fashion shows, photoshoots, events, and workshops.
+          </h1>
+          <h1>
+            <br />We are always working to create an inclusive, uplifting environment where members feel seen, heard, and celebrated — creatively and personally.
+          </h1>
+          <h1>
+            <br />Whether you’re here to build a fashion career, make friends, or just try something new, we’re here to support you in that journey.
+          </h1>
+        </div>
+      </div>
+    </div>
+
   );
 }
