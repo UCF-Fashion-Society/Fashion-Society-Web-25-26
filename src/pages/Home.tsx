57f2d34ea2f8151ae04b7928b22103096d268d2b
Carlos <lopez212318@gmail.com>
2025-10-30 19:03:56 -0400

    Landing page merge done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,43 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="flex justify-center mx-36 mt-4">
+      <div className="flex flex-col">
+         
+          <div className = "relative w-full h-180 bg-center bg-cover bg-[url('/pictures/homePageBackground.JPG')] items-center">
+            <img src="/public/FSTitle.svg" alt="Fashion Society" className="absolute right-5 top-6 w-[1200px] h-auto"/>
+          </div>
+          <div className="w-full h-8 mt-1 text-[#373333] font-['Inter'] text-lg">
+            FASHION SOCIETY FASHION SOCIETY FASHION SOCIETY FASHION SOCIETY FASHION SOCIETY FASHION SOCIETY FASHION SOCIETY 
+          </div>
+          <hr className="w-full border-1" />      
+       </div>
+      <h1 className="flex justify-center mx-36 mt-8 text-lg">
         We want to providing a welcoming space where students can explore styling, design, photography, modeling, or simply fashion as a form of self expression — no experience or major required.
       </h1>
-      <p className="absolute top-[85%] left-[12%] font-bold"> NO EXPERIENCE NEEDED</p>
-      <h1 className="font-bold mt-16 ml-24"> EVERYONE IS WELCOME</h1>
-      <div className="grid grid-cols-2 gap-8">
-        <img
-          src="/OurMission.png"
-          alt="Our Mission"
-          className="w-150 ml-24 "
-        />
-        <img
-          src="/pictures/clownMakeup.svg"
-          alt="temp img"
-          className="ml-56 mt-8"
-        />
+     {/* <p className="absolute top-[85%] left-[12%] font-bold"> NO EXPERIENCE NEEDED</p>  ts ass */}
+      <h1 className="font-bold mt-16 ml-24 text-xl"> EVERYONE IS WELCOME</h1>
+      {/* Second half is here */}
       
+      <div className="grid grid-cols-2 gap-8 pb-20">
+        <div className="">
+          <img
+            src="/OurMission.svg"
+            alt="Our Mission"
+            className="w-200 ml-24 "
+          />
+          <p className="font-bold text-xl mt-20 text-left ml-28">NO EXPERIENCE NEEDED</p>
+        </div>
         <img
-          src="/pictures/clownMakeup.svg"
+          src="/pictures/bride.svg"
           alt="temp img"
-          className="ml-44"
+          className="ml-56 mt-8 "
         />
-        <div className="text-left mr-20 text-[17px] pb-20">
+        <img
+          src="/pictures/phone.svg"
+          alt="temp img"
+          className=" w-100 ml-28"
+        />
+        <div className="text-left mr-20 text-[18px]">
           <h1>
             Fashion Society at UCF is a creative collective and safe space where students of all background come together to express themselves through fashion, art, and culture. whether you dream of working in the fashion industry or simply seek a space to create, connect, and grow — our mission is to nurture individuality, build confidence, and foster authentic community through collaborative experience like fashion shows, photoshoots, events, and workshops.
           </h1>
@@ -35,19 +49,8 @@ export default function Home() {
           </h1>
         </div>
       </div>
+    
     </div>
 
-       <div className="flex flex-col">
-         
-          <div className = "relative w-full h-180 bg-center bg-cover bg-[url('/pictures/homePageBackground.JPG')] items-center">
-            <img src="/public/FSTitle.svg" alt="Fashion Society" className="absolute right-5 top-6 w-[1200px] h-auto"/>
-          </div>
-          <div className="w-full h-8 mt-1 text-[#373333] font-['Inter'] text-lg">
-            FASHION SOCIETY FASHION SOCIETY FASHION SOCIETY FASHION SOCIETY FASHION SOCIETY FASHION SOCIETY FASHION SOCIETY 
-          </div>
-          <hr className="w-full border-1" />
-          <div className="w-full h-100"></div>
-      
-       </div>
   );
 }
